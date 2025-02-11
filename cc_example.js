4b4688caaacd71aa7588097be39ac9cc251d0618
Haley Goodfield <haleygoodfield@usf.edu>
2025-02-11 16:07:29 -0500

    Completed Task 1

---
+++ cc_example.js
@@ -0,0 +1,13 @@
+// Task 1:
+let products = [
+    {name: "Lapotop", price: 1200, category: "Electronics"},
+    {name: "Phone", price: 800, category: "Electronics"},
+    {name: "Desk Chair", price: 150, category: "Furniture"},
+];
+
+// function getProductsByCategory(products, category) {
+   // return products.filter(product => product.category === category);
+// }
+// console.log(getProductsByCategory(products, "Electronics"));
+
+// Task 2
