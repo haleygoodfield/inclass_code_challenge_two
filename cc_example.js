16fede34b05609156eb7af5bb584469cd711b0e1
Haley Goodfield <haleygoodfield@usf.edu>
2025-02-11 16:39:18 -0500

    Final Commit

--- cc_example.js
+++ cc_example.js
@@ -5,31 +5,31 @@ let products = [
     {name: "Desk Chair", price: 150, category: "Furniture"},
 ];
 
-// function getProductsByCategory(products, category) {
-//   return products.filter(product => product.category === category);
-// }
-// console.log(getProductsByCategory(products, "Electronics"));
+ function getProductsByCategory(products, category) {
+   return products.filter(product => product.category === category);
+ }
+ console.log(getProductsByCategory(products, "Electronics"));
 
 // Task 2
 
-// function applyDiscount(products, discountRate) {
-// return products.map(product => ({
-//  ...product, 
-//  price: product.price - (product.price * discountRate),
-//  name: product.name.toUpperCase()
-//  }
-//  )) 
-// }
+ function applyDiscount(products, discountRate) {
+ return products.map(product => ({
+  ...product, 
+  price: product.price - (product.price * discountRate),
+  name: product.name.toUpperCase()
+  }
+  )) 
+ }
 
-// console.log(applyDiscount(products, .1));
+ console.log(applyDiscount(products, .1));
 
-// Task 3
-// let sales = [250, 400, 150, 900, 1200];
-// function calculateTotalRevenue(sales) {
-//    return sales.reduce((total, sale)=>total+sale,0)
-// }
+ // Task 3
+ let sales = [250, 400, 150, 900, 1200];
+ function calculateTotalRevenue(sales) {
+    return sales.reduce((total, sale)=>total+sale,0)
+ }
 
-// console.log(`Total Revenue 2024: $${calculateTotalRevenue(sales)}`);
+ console.log(`Total Revenue 2024: $${calculateTotalRevenue(sales)}`);
 
 // Task 4 
 
